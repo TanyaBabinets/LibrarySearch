8d55b28ca5e78277ea821485c4ac1f0b6fec1cc5
Tanyababinets <tanya060616@gmail.com>
2023-08-04 13:46:42 +0300

    Add project files.

---
+++ LibrarySearch/DbLibrary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LibrarySearch
+{
+    public class DbLibrary
+    {
+        SqlConnection connection = new SqlConnection();
+        DataSet dataSet = new DataSet();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        public DbLibrary()
+        {
+            connection.ConnectionString =
+               ConfigurationManager.ConnectionStrings["library"].ConnectionString;
+
+            InitGroupAdapter();
+        }
+        void InitGroupAdapter()
+        {
+            var selectCommand = connection.CreateCommand();
+            selectCommand.CommandText = "SELECT * FROM Books";
+
+            adapter.SelectCommand = selectCommand;
+            adapter.Fill(dataSet);
+
+            var commandBuilder = new SqlCommandBuilder(adapter);
+            // adapter.InsertCommand = commandBuilder.GetInsertCommand();
+            adapter.UpdateCommand = commandBuilder.GetUpdateCommand();
+            adapter.DeleteCommand = commandBuilder.GetDeleteCommand();
+        }
+
+        public DataTable SearchByName(string n)
+        {
+
+            dataSet.Tables.Clear();
+            var query = connection.CreateCommand();
+            query.CommandText = $"SELECT * FROM Books AS B WHERE B.Name LIKE '%{n}%'";
+            adapter.SelectCommand = query;
+            adapter.Fill(dataSet);
+
+            return dataSet.Tables[0];
+        }
+        public DataTable SearchByAuthor(string n)
+        {
+            dataSet.Tables.Clear();
+            var query = connection.CreateCommand();
+query.CommandText  = $"SELECT B.Name, A.FirstName + ' ' + A.LastName" +
+                $" FROM Books as B join Authors as A on B.Id_Author=A.ID WHERE A.FirstName LIKE '%{n}%' OR A.LastName LIKE '%{n}%'" ;
+                
+            adapter.SelectCommand = query ;
+                adapter.Fill(dataSet);
+
+                return dataSet.Tables[0];
+            
+        }
+        public DataTable SearchByCat(string n)
+        {
+            dataSet.Tables.Clear();
+            var query = connection.CreateCommand();
+            query.CommandText = $"SELECT * FROM Books AS B join Categories AS C on B.Id_Category=C.ID WHERE C.Name LIKE '%{n}%'";
+            adapter.SelectCommand = query;
+            adapter.Fill(dataSet);
+
+            return dataSet.Tables[0];
+        }
+        
+
+    }
+}
---
+++ LibrarySearch/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Использовать БД Library
+// Использовать отсоединенный режим.
+
+//создать приложение для поиска нужной книги по разным критериям:
+//по названию
+//по автору
+//по категории
+
+namespace LibrarySearch
+{
+    public partial class Form1 : Form
+    {
+      
+        DbLibrary db = new DbLibrary();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource=null;
+            if (textBox1.Text != string.Empty)
+            {               
+               
+                dataGridView1.DataSource = db.SearchByAuthor(textBox1.Text);
+                textBox1.Text = string.Empty;
+
+
+            }
+            else if (textBox2.Text != string.Empty)
+            {
+                dataGridView1.DataSource = db.SearchByName(textBox2.Text);
+                textBox2.Text = string.Empty;
+
+
+            }
+            else
+            { 
+                dataGridView1.DataSource = db.SearchByCat(textBox3.Text);
+                textBox3.Text = string.Empty;
+
+
+            }
+        }
+    }
+}
